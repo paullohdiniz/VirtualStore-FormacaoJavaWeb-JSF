074102ea2ed7653788d7f3ee8613593ea6505252
Paulo Diniz <paulo.diniz@emphasysgroup.com.br>
2018-12-26 23:51:51 -0200

    Add da classe UsuarioDAOImpl

---
+++ src/br/com/virtualstore/dao/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package br.com.virtualstore.dao;
+
+import br.com.virtualstore.dao.config.DAO;
+import br.com.virtualstore.domain.usuario.Usuario;
+
+public interface UsuarioDAO extends DAO<Usuario, Integer> {
+		
+}
---
+++ src/br/com/virtualstore/dao/UsuarioDAOImpl.java
@@ -0,0 +1,18 @@
+package br.com.virtualstore.dao;
+
+import br.com.virtualstore.dao.config.DAOImpl;
+import br.com.virtualstore.domain.usuario.Usuario;
+
+public class UsuarioDAOImpl extends DAOImpl<Usuario, Integer> implements UsuarioDAO {
+	
+	
+	
+//	private static final UsuarioDAOImpl instance = new UsuarioDAOImpl();
+//	
+//	public UsuarioDAOImpl(){
+//	}
+//	
+//	public static UsuarioDAOImpl getInstance() {
+//		return instance;
+//	}
+}
